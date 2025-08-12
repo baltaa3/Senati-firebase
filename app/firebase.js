a6240756bbcfcd588fe22e187b9d073906f8cf71
baltaa3 <168559287+baltaa3@users.noreply.github.com>
2025-08-12 10:33:56 -0500

    Add files via upload

---
+++ app/firebase.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAhokGxs_Kst6x4VuMaokdNZMUPHdq4epo",
+    authDomain: "fir-app-dcfbe.firebaseapp.com",
+    projectId: "fir-app-dcfbe",
+    storageBucket: "fir-app-dcfbe.firebasestorage.app",
+    messagingSenderId: "23559212396",
+    appId: "1:23559212396:web:b43d168f122ad29ba53749"
+  };
+
+  // Initialize Firebase
+  export const app = initializeApp(firebaseConfig);
--- main.js
+++ main.js
@@ -1,22 +1,2 @@
+import './app/firebase.js'
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyAxUfZqPlHSCa21vn_tAiubwj761lMYU3Y",
-    authDomain: "logind-e6de0.firebaseapp.com",
-    projectId: "logind-e6de0",
-    storageBucket: "logind-e6de0.firebasestorage.app",
-    messagingSenderId: "400490442127",
-    appId: "1:400490442127:web:a3547c157c366700316ffa",
-    measurementId: "G-V1Z4KSX1S7"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
